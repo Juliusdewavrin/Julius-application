88c1303d81bbc9dc3b9c48b3326b865164c481d1
Julius Dewavrin <jdewavrin@gmail.com>
2016-09-30 19:38:49 +0200

    navbar and front home page edited

--- app/assets/javascripts/cable.js
+++ app/assets/javascripts/cable.js
@@ -3,7 +3,7 @@
 //
 //= require action_cable
 //= require_self
-//= require_tree ./channels
+
 
 (function() {
   this.App || (this.App = {});
@@ -11,3 +11,40 @@
   App.cable = ActionCable.createConsumer();
 
 }).call(this);
+
+
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap
+//= require bootstrap-sprockets
+
+// Neomax JS
+//= require jquery.countTo.js
+//= require jquery.fitvids.js
+//= require jquery.flexslider.js
+//= require jquery.magnific-popup.js
+//= require jquery.singlePageNav.js
+//= require jquery.sticky-kit.js
+//= require jquery.zoom.js
+//= require appear.js
+//= require contact.js
+//= require gmap3.js
+//= require imagesloaded.pkgd.js
+//= require isotope.pkgd.js
+//= require jarallax-video.js
+//= require jarallax.js
+//= require jqBootstrapValidation.js
+//= require owl.carousel.js
+//= require plugins.min.js
+//= require smoothscroll.js
+//= require submenu-fix.js
+//= require twitterFetcher.js
+//= require wow.js
+//= require cloudinary
+//= require custom.min.js
+//= require masonry.pkgd
+
+// at the end
+//= require_tree .
+
+//= require_tree ./channels
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  get 'profile', to: 'pages#profile', as: 'julius'
-  get 'contact', to: 'pages#contact'
+  get 'profile', to: 'pages#profile', as: 'profile'
+  get 'contact', to: 'pages#contact', as: 'contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
