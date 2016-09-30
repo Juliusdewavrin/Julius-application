e697e6a86bcaeb5e7825b04b4a2fd961dc2b2cb4
Julius Dewavrin <jdewavrin@gmail.com>
2016-09-30 17:36:41 +0200

    initial views and routes edited

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,11 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def profile
+  end
+
+  def contact
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'profile', to: 'pages#profile', as: 'julius'
+  get 'contact', to: 'pages#contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
