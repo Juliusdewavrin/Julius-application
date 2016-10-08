bff06ee55eb7ef832b8aa729b436bc42626bddbc
Julius Dewavrin <jdewavrin@gmail.com>
2016-10-08 23:35:50 +0200

    profile page edited - contact section and flexslider

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,30 @@
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap
 //= require bootstrap-sprockets
+
+//= require jquery.countTo.js
+//= require jquery.fitvids.js
+//= require jquery.flexslider.js
+//= require jquery.magnific-popup.js
+//= require jquery.singlePageNav.js
+//= require jquery.sticky-kit.js
+//= require jquery.zoom.js
+//= require appear.js
+//= require contact.js
+//= require gmap3.js
+//= require imagesloaded.pkgd.js
+//= require isotope.pkgd.js
+//= require jarallax-video.js
+//= require jarallax.js
+//= require jqBootstrapValidation.js
+//= require owl.carousel.js
+//= require plugins.min.js
+//= require smoothscroll.js
+//= require submenu-fix.js
+//= require twitterFetcher.js
+//= require wow.js
+//= require custom.min.js
+//= require masonry.pkgd
+
 //= require_tree .
